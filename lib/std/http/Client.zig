4506879a8b1c18a0465e611dc0b3a336d9ddeedc
Igor Anić <igor.anic@gmail.com>
2024-07-22 21:07:49 +0200

    tls: update to changes in tls.zig

--- lib/std/http/Client.zig
+++ lib/std/http/Client.zig
@@ -1356,7 +1356,7 @@ pub fn connectTcp(client: *Client, host: []const u8, port: u16, protocol: Connec
 
         conn.data.tls_client.* = std.crypto.tls23.client(stream, .{
             .host = host,
-            .root_ca = client.ca_bundle,
+            .root_ca = .{ .bundle = client.ca_bundle },
         }) catch return error.TlsInitializationFailed;
     }
 
